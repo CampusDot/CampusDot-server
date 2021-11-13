0ac9e4616ca3fccc1cd0d2cb6fca2e5d0a59f68f
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-13 18:53:23 +0900

    chore: mongodb setting in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require("dotenv").config()
 const app = express()
 const PORT = 3000
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () =>{
+    console.log('Connected to mongo instance');
+});
+db.on('error', (err) => {
+    console.log('Error connecting to mongo', err);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello. This is CampusDot')

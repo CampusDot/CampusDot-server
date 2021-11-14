e37d3a34371d9a810d96876bc4cacd5f779b9615
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-14 13:38:05 +0900

    chore: Add bcrypt and models in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ require("dotenv").config()
 const app = express()
 const PORT = 3000
 
+require('./models/College')
+require('./models/Notice')
+require('./models/Review')
+require('./models/Stamp')
+require('./models/Store')
+require('./models/StoreList')
+require('./models/User')
+
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,6 +29,7 @@ db.on('error', (err) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.send('Hello. This is CampusDot')

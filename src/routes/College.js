a9c2716d620f97d44a13b096668c18e8c5411d82
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-19 15:10:33 +0900

    feat: Add new api related to College (getCollege, getRanking ..)

--- src/routes/College.js
+++ src/routes/College.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 const {
     getStore,
-    getStoreLists
+    getStoreLists,
+    getCollege,
+    getMyCollegeRanking,
+    getOtherCollegeRanking
 } = require('../controllers/College')
 
-router.get('/store/:page', getStore)
+router.get('/store/:category/:page', getStore)
 router.get('/storeLists/:page', getStoreLists)
+router.get('/', getCollege)
+router.get('/myRanking', getMyCollegeRanking)
+router.get('/otherRanking', getOtherCollegeRanking)
+
 module.exports = router

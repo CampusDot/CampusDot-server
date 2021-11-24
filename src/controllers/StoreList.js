dd73d8891f6ce0d217096c0ea3fa7ab77aa9b3f5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-24 17:35:54 +0900

    feat: Add getSelected, challenge StoreList

--- src/controllers/StoreList.js
+++ src/controllers/StoreList.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
 const StoreList = mongoose.model('StoreList');
-const College = mongoose.model('College');
-const User = mongoose.model('User');
 require('date-utils');
-
 
 const postStoreList = async (req, res) => {
     const { Stores, Title, Comment } = req.body;
@@ -32,6 +28,65 @@ const postStoreList = async (req, res) => {
     }
 }
 
+const getSelectedStoreList = async (req, res) => {
+    try {
+        const storeList = await StoreList.aggregate([
+            { $match: { _id: mongoose.Types.ObjectId(req.params.id) }},
+            {
+                $lookup: {
+                    from: 'stores',
+                    localField: 'StoreList',
+                    foreignField: '_id',
+                    as: 'StoreList',
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'PostUser',
+                    foreignField: '_id',
+                    as: 'PostUser'
+                }
+            },
+            {
+                $project: {
+                    Time: 1,
+                    Title: 1,
+                    Comment: 1,
+                    SavedUser: 1,
+                    'PostUser.Name': 1,
+                    'PostUser.ProfileImage': 1,
+                    'PostUser.AllStamp': 1,
+                    'StoreList.Information.name': 1,
+                    'StoreList.Information.photos': 1,
+                    'StoreList.Information.vicinity': 1,
+                }
+            }
+        ])
+        res.status(200).send(storeList[0])
+    } catch (err) {
+        res.status(422).send(err.message)
+    }
+}
+
+const challengeStoreList = async (req, res) => {
+    try {
+        const { id } = req.body
+        await StoreList.findOneAndUpdate({
+            _id: id 
+        }, {
+            $push: {
+                SavedUser: req.user._id
+            }
+        })
+        res.status(200).send(id)
+    } catch (err) {
+        res.status(422).send(err.message)
+    }
+}
+
 module.exports = {
     postStoreList,
+    getSelectedStoreList,
+    challengeStoreList,
 }
--- src/routes/StoreList.js
+++ src/routes/StoreList.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {
     postStoreList,
+    getSelectedStoreList,
+    challengeStoreList,
 } = require('../controllers/StoreList')
 
 router.post('/', postStoreList)
+router.get('/:id', getSelectedStoreList)
+router.post('/challenge', challengeStoreList)
 
 module.exports = router

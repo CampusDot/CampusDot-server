507aef6ff7cb219798146fa26f52447baaa3bcf9
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-25 00:59:00 +0900

    fix: add getReviewStore, getSelectedReview in Review

--- src/routes/Review.js
+++ src/routes/Review.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {
     postReview,
+    getReviewStore,
+    getSelectedReview
 } = require('../controllers/Review')
 
 router.post('/', postReview)
+router.get('/store', getReviewStore)
+router.get('/:id', getSelectedReview)
 
 module.exports = router

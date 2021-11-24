6d415d6ac5da1b2b8e26ea6b9c8942c356d839a9
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-25 00:58:41 +0900

    fix: change url, add getStoreResult in Search

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {
     getCollegeHint,
-    getStoreHint
+    getStoreHint,
+    getStoreResult
 } = require('../controllers/Search')
 
-router.get('/college/:term', getCollegeHint)
-router.get('/store/:term', getStoreHint)
+router.get('/collegeHint/:term', getCollegeHint)
+router.get('/storeHint/:term', getStoreHint)
+router.get('/store/:term', getStoreResult)
 
 module.exports = router

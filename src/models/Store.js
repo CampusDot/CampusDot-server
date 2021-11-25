edb5187d8435d7a3a1e3acbbcc1e5c4538d5110b
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-25 14:16:53 +0900

    remove: remove Review fields in Store

--- src/models/Store.js
+++ src/models/Store.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const storeSchema = new mongoose.Schema({
-    Review: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Review'       
-    }],
     College: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'College'       
